f5293013a7fa8b084782da3e5f263176afde9598
Mark Williams <markrwilliams@gmail.com>
2017-01-15 23:38:41 -0800

    FederationReaderServer logs failures.

--- synapse/app/federation_reader.py
+++ synapse/app/federation_reader.py
@@ -42,6 +42,7 @@ from synapse.crypto import context_factory
 from synapse import events
 
 
+from twisted.logger import Logger
 from twisted.internet import reactor, defer
 from twisted.web.resource import Resource
 
@@ -66,6 +67,8 @@ class FederationReaderSlavedStore(
 
 
 class FederationReaderServer(HomeServer):
+    _log = Logger()
+
     def get_db_conn(self, run_new_connection=True):
         # Any param beginning with cp_ is a parameter for adbapi, and should
         # not be passed to the database engine.
@@ -141,7 +144,8 @@ class FederationReaderServer(HomeServer):
                 result = yield http_client.get_json(replication_url, args=args)
                 yield store.process_replication(result)
             except:
-                logger.exception("Error replicating from %r", replication_url)
+                self._log.failure("Error replicating from {replication_url!r}",
+                                  replication_url=replication_url)
                 yield sleep(5)
 
 
